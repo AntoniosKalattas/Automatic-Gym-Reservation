480065a77075ffddb954e67e40450709a5c9b256
Antonios Kalattas <kalattas66@gmail.com>
2025-01-28 09:55:00 +0200

    Update auto.py

--- auto.py
+++ auto.py
@@ -3,13 +3,21 @@ import time
 import subprocess
 from datetime import datetime, timedelta
 
+
+##########################################################################
+#time options: 1    2    3     4     5     6     7     8                 #
+#              7:45 9:30 11:15 13:00 14:45 16:45 18:30 20:15             #
+              time_option = 7                                            #
+##########################################################################
+
+
 # Function to run the main script
 def run_main_script():
     today = datetime.now()
     future_date = today + timedelta(days=5)
     next5day = future_date.day
     print("Running run.py...")
-    subprocess.run(["python3", "autoReservation.py", "7", str(next5day)])  # Call run.py using subprocess
+    subprocess.run(["python3", "autoReservation.py", str(time_option), str(next5day)])  # Call autoReservation.py using subprocess
 
 # Schedule the script to run every day at 8:00 AM
 schedule.every().day.at("23:37").do(run_main_script)
