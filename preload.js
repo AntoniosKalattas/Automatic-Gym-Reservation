39acd43cd6adefa1501806db8900acd6f4f43aa5
Antonios Kalattas <akalattasmain@gmail.com>
2026-02-26 16:11:12 +0200

    Remove Electron app and web UI files
    Delete the Electron project and its web frontend: remove main process (index.js), preload script, web UI files (web/index.html, web/script.js, web/style.css), image assets, and package.json. Also relocate .DS_Store from img/ to the repository root. This cleans up the repository by removing the Electron application and associated assets.

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  fetchOpenAIData: async () => {
-    const result = await ipcRenderer.invoke('fetch-openai-data');
-    return result;
-  }
-});
